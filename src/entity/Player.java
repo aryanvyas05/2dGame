0a36ac0588dc528eabb1ccf384097a909cba2b5d
Aryan Vyas <aryan.vyas2005@gmail.com>
2024-11-06 14:50:57 -0600

    Increased terrain and centered camera fucntionality

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,6 +1,5 @@
 package entity;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -14,15 +13,21 @@ public class Player extends Entity{
 	GamePanel gp;
 	KeyHandler keyH;
 	
+	public final int screenX;
+	public final int screenY;
+	
 	public Player(GamePanel gp, KeyHandler keyH) {
 		this.gp = gp;
 		this.keyH = keyH;
+		
+		screenX = gp.screenWidth/2 - (gp.tileSize/2);
+		screenY = gp.screenHeight/2 - (gp.tileSize/2);
 		setDefaultValues();
 		getPlayerImage();
 	}
 	
 	public void getPlayerImage() {
-		try {
+		try {		
 			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
 			up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
 			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
@@ -38,8 +43,8 @@ public class Player extends Entity{
 	}
 	
 	public void setDefaultValues() {
-		x = 100;
-		y = 100;
+		worldX = gp.tileSize * 23;
+		worldY = gp.tileSize * 21;
 		speed = 4;	
 		direction = "down";
 	}
@@ -48,22 +53,22 @@ public class Player extends Entity{
 				|| keyH.leftPressed == true || keyH.rightPressed == true) {
 			if(keyH.upPressed == true) {
 				direction = "up";
-				y -= speed;	
+				worldY -= speed;	
 			}
 			if(keyH.downPressed == true) {
 				direction = "down";
-				y += speed;
+				worldY += speed;
 			}
 			if(keyH.leftPressed == true) {
 				direction = "left";
-				x -= speed;
+				worldX -= speed;
 			}
 			if(keyH.rightPressed == true) {
 				direction = "right";
-				x += speed;
+				worldX += speed;
 			}
 			spriteCounter++;
-	        if (spriteCounter > 13) {
+	        if (spriteCounter > 11	) {
 	            if (spriteNumber == 1) {
 	                spriteNumber = 2;
 	            } else {
@@ -91,7 +96,7 @@ public class Player extends Entity{
             image = (spriteNumber == 1) ? right1 : right2;
             break;
     }
-		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+		g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 	}
 	
 	
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,23 +8,30 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
 	final int originalTileSize = 16;
 	final int scale = 3;
 	
 	public final int tileSize = originalTileSize * scale;
-	final int maxScreenColumn = 16;
-	final int maxScreenRow = 12;
+	public final int maxScreenColumn = 16;
+	public final int maxScreenRow = 12;
 	
-	int screenWidth = tileSize * maxScreenColumn;
-	int screenHeight = tileSize * maxScreenRow;
+	public int screenWidth = tileSize * maxScreenColumn;
+	public int screenHeight = tileSize * maxScreenRow;
+	
+	public final int maxWorldCol = 50;
+	public final int maxWorldRow = 50;
+	public final int worldWidth = tileSize * maxWorldCol;
+	public final int worldHeight = tileSize * maxWorldRow;
 	
 	int FPS = 60; 
 	
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
-	Player player = new Player(this, keyH);
+	public Player player = new Player(this, keyH);
+	TileManager tileM = new TileManager(this);			
 	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth,screenHeight));
@@ -68,6 +75,35 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	}
 	
+	/*public void run() {
+		
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+		
+		while(gameThread != null) {			
+			
+			currentTime = System.nanoTime();
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			lastTime = currentTime;
+			if(delta >= 1) {				
+				update();			
+				repaint();
+				delta--;
+				drawCount++;
+			}
+			if(timer >= 1000000000) {
+				System.out.println("FPS: " + drawCount);
+				timer = 0;
+				drawCount = 0;
+			}
+		}
+		
+	}*/
 	public void update() {
 		player.update();
 	}
@@ -76,7 +112,7 @@ public class GamePanel extends JPanel implements Runnable{
 		super.paintComponent(g);
 		
 		Graphics2D g2 = (Graphics2D)g;
-		
+		tileM.draw(g2);
 		player.draw(g2);
 		g2.dispose();
 	}
